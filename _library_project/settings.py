0d2a513c8d37b16fa3dea5c715e4fdb902615d21
UsuallyChris <schoonc@gmail.com>
2019-05-12 18:38:44 -0700

    Installed drf and added to settings.py

--- _library_project/settings.py
+++ _library_project/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third Party apps
+    'rest_framework',
+
     # Local apps
     'books.apps.BooksConfig'
 ]
